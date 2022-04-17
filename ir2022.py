13cdfdea63ee28d84926743967f2f06d739498b9
Andre Muzy <60109618+avmuzy@users.noreply.github.com>
2022-04-16 23:46:54 -0300

    add salario

--- ir2022.py
+++ ir2022.py
@@ -5,3 +5,6 @@ imposto = 0
 if base > 3000:
      imposto = imposto + ((base - 3000) * 0.35)
      base = 3000
+if base > 1000:
+     imposto = imposto + ((base - 1000) * 0.20)
+print("Salário: R$%6.2f Imposto a pagar: R$%6.2f" % (salário, imposto))
