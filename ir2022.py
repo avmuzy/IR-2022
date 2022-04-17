b3604911587b82e35f2fcaf51064ed340ae02278
Andre Muzy <60109618+avmuzy@users.noreply.github.com>
2022-04-16 23:46:10 -0300

    add salario

--- ir2022.py
+++ ir2022.py
@@ -1,2 +1,7 @@
 
 salário = float(input("Digite o salário para cálculo do imposto: "))
+base = salário
+imposto = 0
+if base > 3000:
+     imposto = imposto + ((base - 3000) * 0.35)
+     base = 3000
