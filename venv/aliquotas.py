09d489089b5e46fa823f849b733edf8aacf13409
Andre Muzy <60109618+avmuzy@users.noreply.github.com>
2022-04-19 19:54:03 -0300

    simplifying  if comparison

--- venv/aliquotas.py
+++ venv/aliquotas.py
@@ -3,11 +3,11 @@ base = salário
 imposto = 0
 if base < 2500:
     print('Isento')
-elif base >= 2500 and base < 3200:
+elif 2500 <= base < 3200:
     print('Aliquota de 7,5%')
-elif base >= 3200 and base < 4200:
+elif 3200 <= base < 4200:
     print('Aliquota de 15%')
-elif base >= 4200 and base < 5300:
+elif 4200 <= base < 5300:
     print('Aliquota de 22,5%')
-elif base >= 5300:
+else:
     print('Aliquota de 27,5%')
